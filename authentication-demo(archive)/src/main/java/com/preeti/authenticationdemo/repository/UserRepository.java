a83f627498a03c674964e0874d0d7160c04c05ff
Preeti Tewatia <preetitewatia9467@gmail.com>
2026-07-21 13:00:45 +0530

    Removed Archived Authentication-demo project

--- authentication-demo(archive)/src/main/java/com/preeti/authenticationdemo/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.preeti.authenticationdemo.repository;
-
-import com.preeti.authenticationdemo.model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String> {
-
-}
